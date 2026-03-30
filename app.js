65727bdae734fbc010db9efaa698ba58d85fa734
imanuj07 <anujsahu.357@gmail.com>
2026-03-30 14:09:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('7thhh updateee Hello World');
+    res.end('8thhh updateee Hello World');
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
