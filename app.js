d7aca5e048499d1134ba1598d6fc30e99700c4e4
Anuj Sahu <19492361+imanuj07@users.noreply.github.com>
2026-04-01 10:26:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('gitcheckkkk emailprivate usernamess repo name connected ho gaya 13thhh updateee Hello World');
+    res.end('gitcheckkkk emailprivate path repo name connected ho gaya 13thhh updateee Hello World');
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
